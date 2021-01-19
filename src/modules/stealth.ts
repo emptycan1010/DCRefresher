b85713599ceb57ac79620eb2b637be9ae5151333
Sochiru <unleash9224@gmail.com>
2021-01-19 09:40:10 +0900

    리펙토링, 버튼 사이즈 줄이기

--- src/modules/stealth.ts
+++ src/modules/stealth.ts
@@ -2,36 +2,43 @@ const getURL = (u: string) => {
   return !chrome || !chrome.extension ? u : chrome.extension.getURL(u)
 }
 
-const tempButtonCreate = (elem:HTMLElement):void =>{
-  const buttonNum:number = elem.querySelectorAll('.stealth_control_button').length
-  const contentNum:number = elem.querySelectorAll('.write_div img, .write_div video').length;
+const CONTROL_BUTTON = '.stealth_control_button'
+const TEMPORARY_STEALTH = 'stlth'
 
-  if(buttonNum == 0 && contentNum != 0){
-    let buttonFrame:HTMLElement = document.createElement('div');
-    buttonFrame.classList.add('stealth_control_button');   
-    buttonFrame.classList.add('blur');   
+const tempButtonCreate = (elem: HTMLElement): void => {
+  const buttonNum: number = elem.querySelectorAll(CONTROL_BUTTON).length
+  const contentNum: number = elem.querySelectorAll(
+    '.write_div img, .write_div video'
+  ).length
+
+  if (buttonNum == 0 && contentNum != 0) {
+    let buttonFrame: HTMLElement = document.createElement('div')
+    buttonFrame.classList.add(CONTROL_BUTTON.replace('.', ''))
+    buttonFrame.classList.add('blur')
 
     buttonFrame.innerHTML = `      
       <div class="button" id ="tempview">
-      <img src="${getURL('/assets/icons/change.png')}"></img>
-      <p id="temp_button_text">이미지 보이기</p>
+        <img src="${getURL('/assets/icons/change.png')}"></img>
+        <p id="temp_button_text">이미지 보이기</p>
       </div>
     `
 
-    let button = buttonFrame.querySelector('#tempview') as HTMLElement;
-    let buttonText = buttonFrame.querySelector('#temp_button_text') as HTMLElement;
+    let button = buttonFrame.querySelector('#tempview') as HTMLElement
+    let buttonText = buttonFrame.querySelector(
+      '#temp_button_text'
+    ) as HTMLElement
 
-    button.addEventListener('click', ev => {
-      if(elem.className.indexOf('stlth') < 0){
-        elem.classList.add('stlth')
-        buttonText.innerText = '이미지 숨기기';
-      }else{
-        elem.classList.remove('stlth')
-        buttonText.innerText = '이미지 보이기';
+    button.addEventListener('click', _ => {
+      if (elem.className.indexOf(TEMPORARY_STEALTH) < 0) {
+        elem.classList.add(TEMPORARY_STEALTH)
+        buttonText.innerText = '이미지 숨기기'
+      } else {
+        elem.classList.remove(TEMPORARY_STEALTH)
+        buttonText.innerText = '이미지 보이기'
       }
     })
 
-    elem.prepend(buttonFrame);   
+    elem.prepend(buttonFrame)
   }
 }
 
@@ -45,32 +52,31 @@ export default {
   },
   enable: true,
   default_enable: true,
-  require: ['filter', 'eventBus'],
-  func (
-    filter: RefresherFilter, 
-    eventBus: RefresherEventBus,
-  ) {
+  require: ['eventBus'],
+  func (eventBus: RefresherEventBus) {
     if (
       document &&
       document.documentElement &&
       document.documentElement.className.indexOf('refresherStealth') < 0
     ) {
       document.documentElement.classList.add('refresherStealth')
-    }    
+    }
 
-    if(document.querySelectorAll('.stealth_control_button').length == 0){
+    if (!document.querySelectorAll(CONTROL_BUTTON).length) {
       tempButtonCreate(document.documentElement)
     }
-  
-    this.memory.contentViewUUID = eventBus.on('contentPreview', (elem: HTMLElement)=>{
-      if(elem.querySelectorAll('.stealth_control_button').length == 0){
-        tempButtonCreate(elem)
-      }
-    })
 
+    this.memory.contentViewUUID = eventBus.on(
+      'contentPreview',
+      (elem: HTMLElement) => {
+        if (!elem.querySelectorAll(CONTROL_BUTTON).length) {
+          tempButtonCreate(elem)
+        }
+      }
+    )
   },
 
-  revoke(filter: RefresherFilter, eventBus: RefresherEventBus){
+  revoke (eventBus: RefresherEventBus) {
     document.documentElement.classList.remove('refresherStealth')
 
     if (this.memory.contentViewUUID) {
