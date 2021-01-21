34177c42a16e9130a4972129fdd70dcf78bf1a9d
Seohyun Lee <04seohyun@gmail.com>
2021-01-22 01:22:20 +0900

    Add option to change font

--- src/modules/fonts.ts
+++ src/modules/fonts.ts
@@ -4,7 +4,8 @@ export default {
   author: { name: 'Sochiru', url: '' },
   status: {
     customFonts: 'Noto Sans CJK KR, NanumGothic',
-    changeDCFont: true
+    changeDCFont: true,
+    bodyFontSize: 13
   },
   memory: {
     uuid: null
@@ -20,29 +21,41 @@ export default {
     changeDCFont: {
       name: '디시인사이드 폰트 교체',
       desc:
-        '미리보기 창 같은 DCRefresher의 폰트 뿐만 아니라 디시인사이드의 폰트까지 교체합니다.',
+          '미리보기 창 같은 DCRefresher의 폰트 뿐만 아니라 디시인사이드의 폰트까지 교체합니다.',
       type: 'check',
       default: true
+    },
+    bodyFontSize: {
+      name: '본문 폰트 크기 지정',
+      desc:
+          '본문의 기본 폰트 크기를 조정합니다.',
+      type: 'range',
+      default: 13,
+      min: 5,
+      step: 1,
+      max: 30,
+      unit: 'pt',
     }
   },
 
   update: {
-    customFonts: (value: string | boolean) => {
+    customFonts: (fontName: string | boolean, fontSize: number) => {
       let fontElement = document.querySelector('#refresherFontStyle')
-      if (fontElement && !value) {
+      if (fontElement && !fontName) {
         fontElement.parentElement?.removeChild(fontElement)
 
         return
       }
 
-      if (value && document && document.head) {
+      if (fontName && document && document.head) {
         if (!fontElement) {
           fontElement = document.createElement('style')
           fontElement.id = 'refresherFontStyle'
           document.head.appendChild(fontElement)
         }
 
-        fontElement.innerHTML = `.refresherChangeDCFont,.refresherChangeDCFont body,.refresherFont .refresher-block-popup,.refresherFont .refresher-frame,.refresherFont .refresher-popup,.refresherChangeDCFont .gall_list,.refresherChangeDCFont button,.refresherChangeDCFont input,.refresherChangeDCFont .view_comment div,.refresherChangeDCFont .view_content_wrap,.refresherChangeDCFont .view_content_wrap a,.refresherChangeDCFont .btn_cmt_close,.refresherChangeDCFont .btn_cmt_close span,.refresherChangeDCFont .btn_cmt_refresh,.refresherChangeDCFont .btn_cmt_open{font-family:${value},-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Open Sans','Helvetica Neue',sans-serif!important}`
+        fontElement.innerHTML = `.refresherChangeDCFont,.refresherChangeDCFont body,.refresherFont .refresher-block-popup,.refresherFont .refresher-frame,.refresherFont .refresher-popup,.refresherChangeDCFont .gall_list,.refresherChangeDCFont button,.refresherChangeDCFont input,.refresherChangeDCFont .view_comment div,.refresherChangeDCFont .view_content_wrap,.refresherChangeDCFont .view_content_wrap a,.refresherChangeDCFont .btn_cmt_close,.refresherChangeDCFont .btn_cmt_close span,.refresherChangeDCFont .btn_cmt_refresh,.refresherChangeDCFont .btn_cmt_open{font-family:${fontName},-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Open Sans','Helvetica Neue',sans-serif!important}`
+        fontElement.innerHTML += `.refresherChangeDCFont .write_div, .refresherChangeDCFont .refresher-preview-contents-actual {font-size: ${fontSize}px;}`
       }
     },
     changeDCFont: (vaule: boolean) => {
@@ -57,12 +70,12 @@ export default {
   func () {
     document.documentElement.classList.add('refresherFont')
     this.update.changeDCFont(this.status.changeDCFont)
-    this.update.customFonts(this.status.customFonts)
+    this.update.customFonts(this.status.customFonts, this.status.bodyFontSize)
   },
 
   revoke () {
     document.documentElement.classList.remove('refresherFont')
     this.update.changeDCFont(false)
-    this.update.customFonts(false)
+    this.update.customFonts(false, 13)
   }
 }
