fd50343c311f9a14a1ffd817b131bb62a8ea7d5d
So-chiru <unleash9224@gmail.com>
2021-02-18 12:37:45 +0900

    fix: ignore none-dc page

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,34 @@ import './styles/index.scss'
 
 import log from './utils/logger'
 
-log('🍊⚓ Initializing DCRefresher.')
-
-const loadStart = performance.now()
-
 import './core/block'
 import { modules } from './core/modules'
 import { filter } from './core/filtering'
 
 import './core/updateCheck'
+;(() => {
+  if (!/.+\.dcinside\.com/.test(location.hostname)) {
+    return
+  }
 
-const context = require.context('./modules/', true, /\.ts$/)
-Promise.all(context.keys().map((v: string) => context(v).default))
-  .then((v: RefresherModule[]) => modules.load(...v))
-  .then(async () => {
-    log(
-      `🍊✔️ DCRefresher Module Loaded. took ${(
-        performance.now() - loadStart
-      ).toFixed(2)}ms.`
-    )
+  log('🍊⚓ Initializing DCRefresher.')
 
+  const loadStart = performance.now()
+
+  const context = require.context('./modules/', true, /\.ts$/)
+  Promise.all(context.keys().map((v: string) => context(v).default))
+    .then((v: RefresherModule[]) => modules.load(...v))
+    .then(async () => {
+      log(
+        `🍊✔️ DCRefresher Module Loaded. took ${(
+          performance.now() - loadStart
+        ).toFixed(2)}ms.`
+      )
+
+      filter.run()
+    })
+
+  window.addEventListener('load', async () => {
     filter.run()
   })
-
-window.addEventListener('load', async () => {
-  filter.run()
-})
+})()
